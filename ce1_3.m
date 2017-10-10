a412a3f92ac16b38800b3d61a29d2b3f92498747
Michael Spieler <michael.spieler@epfl.ch>
2017-10-10 11:42:11 +0200

    CE1 numerical deconvolution

--- ce1_3.m
+++ ce1_3.m
@@ -1,16 +1,28 @@
 close all;
 clc;
 
-a = -0.2;
-b = 0.2;
+a = -0.4;
+b = -a;
 Te = 0.2;
-N = 50/Te;
+sim_time = 100; % seconds
+N = sim_time/Te;
 
-signal = a + (b-a).*rand(N,1);
+u = a + (b-a).*rand(N,1);
 
 simin = struct();
-simin.signals = struct('values', signal);
+simin.signals = struct('values', u);
 simin.time = linspace(0,N*Te, N);
 
 sim('ce1_1_sim')
+plot(simout)
 
+U = toeplitz(u, zeros(N,1));
+Y = simout.Data;
+
+k = 300;
+Uk = U(:,1:k);
+
+theta = pinv(Uk)*Y;
+
+%theta = U\Y;
+plot(theta);
