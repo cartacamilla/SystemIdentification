e0c899a3f78c6bad5c037b24e93a02cec2612283
milla93 <carta.camilla.93@gmail.com>
2017-10-17 12:04:46 +0200

    today's session

--- ce1_4.m
+++ ce1_4.m
@@ -2,18 +2,31 @@ close all;
 hold off;
 
 % input signal
-Uprbs = prbs(4,7);
+Uprbs = 0.5* prbs(7,4);
 
 % sample time
 Te = 0.2;
 
-sim_time = 100; % seconds
 N = length(Uprbs);
+sim_time = N*Te; % seconds
 
 simin = struct();
 simin.signals = struct('values', Uprbs);
-simin.time = linspace(0,N*Te, N);
+simin.time = linspace(0,sim_time, N);
 
 sim('ce1_1_sim')
 plot(simout)
 
+Ruu = intcor(Uprbs, Uprbs);
+Ruu_x = xcorr(Uprbs, Uprbs);
+
+Y = simout;
+
+Ryu = intcor(Y, Uprbs);
+Ryu_x = xcorr(Y, Uprbs)
+
+U = toeplitz(Ruu(1:N,1));
+
+theta = pinv(U)*Ryu(3*N:length(Ryu),1);
+
+plot(theta)
