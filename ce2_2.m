c7353f910f5efc08a2333152fa1df90a7990422c
Michael Spieler <michael.spieler@epfl.ch>
2017-11-12 22:47:39 +0100

    cleanup CE2.2, save plots to pdf

--- ce2_2.m
+++ ce2_2.m
@@ -1,11 +1,11 @@
-% input signal
+
+% simulation parameters
 saturation = 0.5;
 noiseVariance = 0.1;
-%noiseVariance = 0;
 
-N_PERIODS = 1;
-u = 0.5*prbs(10,N_PERIODS);
-PERIOD_LEN = length(u)/N_PERIODS;
+% input signal
+u = 0.5*prbs(10,1);
+PERIOD_LEN = length(u);
 Te = 0.2; % sample time
 N = length(u);
 sim_time = N*Te;
@@ -16,6 +16,10 @@ simin.signals = struct('values', u);
 simin.time = linspace(0,N*Te, N);
 sim('ce1_1_sim')
 y = simout;
+
+% true system
+G = tf([4],[1 1 4]);
+Z = c2d(G, Te, 'zoh');
 
 
 %% Spectral analysis method
@@ -50,23 +54,27 @@ Gr = Gr(1:NYQUIST_INDEX);
 model_hamming = frd(Gr, freq, Te);
 
 window = [hann(WINDOW_SIZE); padding];
-Ryu_hann = Ryu.*[hann(WINDOW_SIZE); padding];
+Ryu_hann = Ryu.*window;
 Gr = fft(Ryu_hamming)./fft(Ruu);
 Gr = Gr(1:NYQUIST_INDEX);
 model_hann = frd(Gr, freq, Te);
 
-bode(model)
-
-% compare with true system
+% Bode plot
+figure
 hold on
 bode(model)
-bode(model_hann)
-bode(model_hamming)
-G = tf([4],[1 1 4]);
-Z = c2d(G, Te, 'zoh');
 bode(Z,freq)
-%legend('no window','Hann','Hamming','System')
+title('Bode Diagram: whole data (no window)')
 hold off
+printpdf(gcf, 'ce2_2_no_window.pdf');
+
+figure
+hold on
+bode(model_hann)
+bode(Z,freq)
+title('Bode Diagram: Hann window')
+hold off
+printpdf(gcf, 'ce2_2_hann_window.pdf');
 
 %% averaging
 M_DIV = 10;
@@ -74,7 +82,7 @@ CHUNK_LEN = floor(PERIOD_LEN/M_DIV);
 
 WINDOW_SIZE = 40;
 padding = zeros(CHUNK_LEN - WINDOW_SIZE, 1);
-window = [hamming(WINDOW_SIZE); padding];
+window = [hann(WINDOW_SIZE); padding];
 
 fft_Ryu = zeros(CHUNK_LEN,1);
 fft_Ruu = zeros(CHUNK_LEN,1);
@@ -102,13 +110,13 @@ omega_s = 2*pi/Te;
 freq = 0:omega_s/CHUNK_LEN:(CHUNK_LEN-1)/CHUNK_LEN*omega_s;
 freq = freq(1:NYQUIST_INDEX);
 
-model = frd(Gr, freq, Te);
+model_avg = frd(Gr, freq, Te);
 
 figure
 hold on
-bode(model)
-
-G = tf([4],[1 1 4]);
-Z = c2d(G, Te, 'zoh');
+bode(model_avg)
 bode(Z,freq)
+title('Bode Diagram: Averaging')
 hold off
+printpdf(gcf, 'ce2_2_averaging.pdf');
+
