387530c4fef7b6daa518012796b1b389eec7e2cf
Michael Spieler <michael.spieler@epfl.ch>
2017-11-20 23:12:50 +0100

    CE2 plot generation

--- ce2_2.m
+++ ce2_2.m
@@ -1,5 +1,3 @@
-SCALEOPT = 'biased';
-
 % simulation parameters
 saturation = 0.5;
 noiseVariance = 0.1;
@@ -38,7 +36,7 @@ figure
 hold on
 bode(model)
 bode(Z,model.Frequency)
-title('Bode Diagram: whole data (no window)')
+title('Spectral analysis: whole data (no window)')
 hold off
 printpdf(gcf, 'ce2_2_no_window.pdf');
 
@@ -46,12 +44,12 @@ figure
 hold on
 bode(model_hann)
 bode(Z,model_hann.Frequency)
-title('Bode Diagram: Hann window')
+title('Spectral analysis: Hann window')
 hold off
 printpdf(gcf, 'ce2_2_hann_window.pdf');
 
 %% Averaging
-N_AVG = 10;
+N_AVG = 8;
 
 % averaging without window
 model = spectral_analysis_avg(y,u,Te,N_AVG,'biased');
@@ -61,18 +59,56 @@ window = hann(40);
 model_hann = spectral_analysis_avg(y,u,Te,N_AVG,'biased',window);
 
 figure
+subplot(1,2,1)
 hold on
 bode(model)
 bode(Z,model.Frequency)
-title('Bode Diagram: Averaging')
+title('Spectral analysis: Averaging')
 hold off
-
-figure
+subplot(1,2,2)
 hold on
 bode(model_hann)
-bode(Z,model_hann.Frequency)
-title('Bode Diagram: Averaging with Hann window')
+bode(Z,model.Frequency)
+title('Spectral analysis: Averaging with Hann window')
 hold off
 
-printpdf(gcf, 'ce2_2_averaging.pdf');
+printpdf(gcf, 'ce2_2_averaging.pdf',1.5,1);
+
+%% unbiased plots
+figure
+subplot(2,2,1)
+hold on
+model_biased = spectral_analysis(y,u,Te,'biased');
+model_unbiased = spectral_analysis(y,u,Te,'unbiased');
+bode(model_biased)
+bode(model_unbiased)
+bode(Z,model_biased.Frequency)
+title('Spectral analysis: whole data')
+legend('biased','unbiased','true model')
+hold off
+
+window = hann(40);
+subplot(2,2,2)
+hold on
+model_biased = spectral_analysis(y,u,Te,'biased',window);
+model_unbiased = spectral_analysis(y,u,Te,'unbiased',window);
+bode(model_biased)
+bode(model_unbiased)
+bode(Z,model_biased.Frequency)
+title('Spectral analysis: Windowing')
+legend('biased','unbiased','true model')
+hold off
+
+subplot(2,2,3)
+hold on
+model_biased = spectral_analysis_avg(y,u,Te,N_AVG,'biased');
+model_unbiased = spectral_analysis_avg(y,u,Te,N_AVG,'unbiased');
+bode(model_biased)
+bode(model_unbiased)
+bode(Z,model_biased.Frequency)
+title('Spectral analysis: Averaging')
+legend('biased','unbiased','true model')
+hold off
+
+printpdf(gcf, 'ce2_2_unbiased.pdf',1.5,2);
 
