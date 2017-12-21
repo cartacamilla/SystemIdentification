f711d60a5ce05eaef6d14d06bd465c319a23bbfa
Michael Spieler <michael.spieler@epfl.ch>
2017-12-21 16:29:07 +0100

    final: work on spectral & fourier analysis

--- final_project.m
+++ final_project.m
@@ -10,10 +10,10 @@ N = length(FPdata.u)/6;
 k=250;
 u = FPdata.u(end-N+1:end);
 y = FPdata.y(end-N+1:end);
-Ruu = intcor(u, u); % use incor because u periodic
-Ryu = intcor(y, u);
-U = toeplitz(Ruu(N:N+k));
-theta = pinv(U'*U)*U'*Ryu(N:N+k);
+Rur = intcor(u, u); % use incor because u periodic
+Ryr = intcor(y, u);
+U = toeplitz(Rur(N:N+k));
+theta = pinv(U'*U)*U'*Ryr(N:N+k);
 
 figure
 plot(1/Te*theta)
@@ -29,27 +29,75 @@ hold off
 %% spectral and fourier analyis
 u = FPdata.u;
 y = FPdata.y;
-N = length(y);
-data = iddata(y,u,Te);
+r = FPdata.r;
 
-model = spectral_analysis(y,u,Te,'biased',hamming(10));
+N_AVG = 6;
+N = floor(length(u)/N_AVG);
+
+SCALEOPT = 'biased';
+window = ones(N,1);
+window = hann(200);
+window = window(length(window)/2 +1:end);
+padding = zeros(N - length(window), 1);
+window = [window; padding];
+
+fft_Ryr = zeros(N,1);
+fft_Rur = zeros(N,1);
+for i = 0:N_AVG-1
+    % split into chunks
+    yc = y(i*N + 1:(i+1)*N);
+    uc = u(i*N + 1:(i+1)*N);
+    rc = r(i*N + 1:(i+1)*N);
+
+    % correlation
+    Ryr = xcorr(yc,rc, SCALEOPT);
+    Rur = xcorr(uc,rc, SCALEOPT);
+
+    Ryr = Ryr(N:end);
+    Rur = Rur(N:end);
+
+    % windowing
+    Ryr = Ryr .* window;
+
+    % averaging
+    fft_Ryr = fft_Ryr+fft(Ryr);
+    fft_Rur = fft_Rur+fft(Rur);
+end
+
+% Reconstruction
+Gr = fft_Ryr./fft_Rur;
+
+omega_s = 2*pi/Te;
+freq = 0:omega_s/N:(N-1)/N*omega_s;
+
+NYQUIST_INDEX = round(N/2);
+Gr = Gr(1:NYQUIST_INDEX);
+freq = freq(1:NYQUIST_INDEX);
+
+model = frd(Gr, freq, Te);
+figure
 bode(model)
 
-%%% fourier analyis
+%% fourier analyis
+u = FPdata.u;
+y = FPdata.y;
+
+N = length(FPdata.u);
 N_PERIODS = 6;
 PERIOD_LEN = N/N_PERIODS;
 omega_s = 2*pi/Te;
-avg = zeros(PERIOD_LEN,1);
+avg_y = zeros(PERIOD_LEN,1);
+% todo avg_u
 for i = 1:PERIOD_LEN:N
     sig = y(i:i+PERIOD_LEN-1);
-    avg = avg + fft(sig);
+    avg_y = avg_y + fft(sig);
 end
 freq = [];
 for i = 0:PERIOD_LEN-1
    freq = [freq; i*omega_s/127];
 end
 
-Y = avg / N_PERIODS;
+Y = avg_y / N_PERIODS;
 U = fft(u(1:PERIOD_LEN));
 
 % Reconstruction
@@ -69,6 +117,7 @@ bode(model)
 
 u = FPdata.u;
 y = FPdata.y;
+r = FPdata.r;
 N = length(y);
 
 data = iddata(y(1:N/2),u(1:N/2),Te);
